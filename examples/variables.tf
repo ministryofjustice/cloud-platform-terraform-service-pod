066c7992663420ff612b36ea90bd34c48865dd3c
jakemulley <jakemulley@users.noreply.github.com>
2023-07-11 09:59:57 +0100

    Implement initial kubernetes_deployment for cloud-platform-terraform-service-pod

---
+++ examples/variables.tf
@@ -0,0 +1,31 @@
+variable "eks_cluster_name" {
+  default = "live"
+}
+
+variable "business_unit" {
+  default = "Platforms"
+}
+
+variable "application" {
+  default = "cloud-platform-terraform-service-pod example module"
+}
+
+variable "is_production" {
+  default = "false"
+}
+
+variable "team_name" {
+  default = "webops"
+}
+
+variable "namespace" {
+  default = "cloud-platform-terraform-service-pod-example-module"
+}
+
+variable "environment" {
+  default = "non-production"
+}
+
+variable "infrastructure_support" {
+  default = "Cloud Platform"
+}
--- main.tf
+++ main.tf
@@ -1,2 +1,66 @@
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
+locals {
+  # Generic configuration
+  identifier = "cloud-platform-${random_id.name.hex}"
+}
+
+########################
+# Generate identifiers #
+########################
+resource "random_id" "name" {
+  byte_length = 8
+}
+
+#########################
+# Get service pod image #
+#########################
+data "aws_ecr_repository" "service_pod" {
+  name = "webops/cloud-platform-service-pod"
+}
+
+################################
+# Create Kubernetes deployment #
+################################
+resource "kubernetes_deployment" "service_pod" {
+  metadata {
+    name      = "${local.identifier}-service-pod"
+    namespace = var.namespace
+
+    labels = {
+      app = "service-pod"
+    }
+  }
+
+  spec {
+    replicas = var.service_pod_count
+
+    selector {
+      match_labels = {
+        app = "${local.identifier}-service-pod"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "${local.identifier}-service-pod"
+        }
+      }
+
+      spec {
+        service_account_name = var.service_account_name
+        container {
+          image = "${data.aws_ecr_repository.service_pod.repository_url}:c5f69b4624b956248001fa7c173c89a0556a457e" # update this hash on a new Dockerfile release
+          name  = "service-pod"
+          stdin = true
+          tty   = true
+
+          security_context {
+            allow_privilege_escalation = false
+            run_as_non_root            = true
+            run_as_user                = 1001
+          }
+        }
+      }
+    }
+  }
+}
