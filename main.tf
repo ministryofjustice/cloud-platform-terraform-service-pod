77d93f072c3b4ac144e0650c9ce6d88ae692ba2f
dan-criddle <dan.criddle@justice.gov.uk>
2026-08-27 11:31:08 +0100

    Chore: Update Image Tag For Docker13 Image

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "kubernetes_deployment" "service_pod" {
       spec {
         service_account_name = var.service_account_name
         container {
-          image = "${data.aws_ecr_repository.service_pod.repository_url}:f9619b99ffe9ae511dde5e7e6e5b67ea289bbab5" # update this hash on a new Dockerfile release
+          image = "${data.aws_ecr_repository.service_pod.repository_url}:7603c88995a3b8897e8383285f7536cc4b577f4e" # update this hash on a new Dockerfile release
           name  = "service-pod"
           stdin = true
           tty   = true
